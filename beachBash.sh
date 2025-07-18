d9bc246ffa75e4d11f2f019415c3daeaee777336
magrathea-bound <dg.burns@outlook.com>
2025-07-18 13:47:45 -0500

    added fzn and gs

--- beachBash.sh
+++ beachBash.sh
@@ -6,3 +6,5 @@ export PS1='[\[\e[1;32m\]\u@\h\[\e[0m\] \[\e[1;34m\]\W\[\e[0m\]]\$ '
 # Aliases
 alias dd='cd'
 alias fzd='cd $(find /home/bravedave -type d | fzf)'
+alias fzn='nvim $(find /home/bravedave -type d | fzf)'
+alias gs='git status --porcelain'

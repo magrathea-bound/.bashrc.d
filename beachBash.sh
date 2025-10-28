c4e5cf2115d388d029c0ee984aac1ae1c93eb6b2
magrathea-bound <dg.burns@outlook.com>
2025-10-28 16:28:37 -0500

    Adding hot keys to jump directories quicker

--- beachBash.sh
+++ beachBash.sh
@@ -5,6 +5,22 @@ export PS1='[\[\e[1;32m\]\u@\h\[\e[0m\] \[\e[1;34m\]\W\[\e[0m\]]\$ '
 
 # Aliases
 alias dd='cd'
-alias fzd='cd $(find /home/bravedave -type d | fzf)'
-alias fzn='nvim $(find /home/bravedave -type d | fzf)'
 alias gs='git status --porcelain'
+
+# Jump commands
+fzd() {
+    alias fzd='cd $(find /home/bravedave -type d | fzf)'
+}
+bind -x '"\C-j": fzd'
+
+fzn() {
+    local dir
+    dir="$(find /home/bravedave/ -type d 2>/dev/null | fzf)" || return
+    [ -n  "$dir" ] || return
+    cd "$dir" || return
+    nvim .
+}
+bind -x '"\C-n": fzn'
+
+bind -x '"\C-u":cd ..'
+
